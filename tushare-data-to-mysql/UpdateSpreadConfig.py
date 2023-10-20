8a495d67c1c9054ef2972a6c5bf64c47e38e8cd9
Yansea Tan <yanseatan@163.com>
2023-10-20 17:24:41 +0800

    update json decode

--- tushare-data-to-mysql/UpdateSpreadConfig.py
+++ tushare-data-to-mysql/UpdateSpreadConfig.py
@@ -18,16 +18,23 @@ def update_spread_config():
     engine_ts = creat_engine_with_database('futures')
     for i in range(0, len(ops_json)):
         fut_code = ops_json[i]['ProductID']
+        ops_json[i].pop("RecommendSpread")
         sql = "select distinct spread_type from fut_spread_daily where fut_code = '{}' order by spread_type".format(fut_code)
         spread_type_df = read_data(engine_ts, sql)
-        spread_dict = {}
+        spread_type_list = []
+        for j in range(0, len(spread_type_df)):
+            spread_type_list.append(spread_type_df.loc[j]['spread_type'])
+        spread_price_list = []
         for j in range(0, len(spread_type_df)):
             spread_type = spread_type_df.loc[j]['spread_type']
             sql = "select close from fut_spread_daily where fut_code = '{}' and spread_type = '{}' order by close".format(fut_code, spread_type)
             close_df = read_data(engine_ts, sql)
             num = len(close_df)
-            spread_dict[spread_type] = close_df.loc[max(round(num * 0.1), 1) - 1]['close']
-        ops_json[i]["RecommendSpread"] = spread_dict
+            spread_price_list.append(close_df.loc[max(round(num * 0.1), 1) - 1]['close'])
+        # spread_dict = {}
+        ops_json[i]['SpreadType'] = spread_type_list
+        ops_json[i]['RecPrice'] = spread_price_list
+        # ops_json[i]["RecommendSpread"] = spread_dict
         print('{} 价差配置写入成功，文件更新进度：{}%'.format(fut_code, format(i / len(ops_json) * 100, '.2f')))
     
     f = open('./productOps.json', 'w')
