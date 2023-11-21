9ce9fcde4afdfd73528254f041e2d143d25deb2c
Yansea Tan <yanseatan@163.com>
2023-11-21 10:47:57 +0800

    update, add WarrantsLogoutMonth data

--- tushare-data-to-mysql/UpdateSpreadConfig.py
+++ tushare-data-to-mysql/UpdateSpreadConfig.py
@@ -2,7 +2,7 @@
 # @Author: Yansea
 # @Date:   2023-10-18
 # @Last Modified by:   Yansea
-# @Last Modified time: 2023-11-20
+# @Last Modified time: 2023-11-21
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -20,19 +20,21 @@ def update_spread_config():
     engine_ts = creat_engine_with_database('futures')
     for i in range(0, len(ops_json)):
         fut_code = ops_json[i]['ProductID']
+        # fut_code = 'CJ'
         sql = "select distinct spread_type from fut_spread_daily where fut_code = '{}' order by spread_type".format(fut_code)
         spread_type_df = read_data(engine_ts, sql)
         spread_type_list = []
         spread_price_list = []
         for j in range(0, len(spread_type_df)):
             spread_type = spread_type_df.loc[j]['spread_type']
+            # spread_type = '03-05'
             sql = "select distinct ts_code from fut_spread_daily where fut_code = '{}' and spread_type = '{}' order by ts_code".format(fut_code, spread_type)
             ts_code_df = read_data(engine_ts, sql)
             num_of_trade_date = 0
             close_df = pd.DataFrame()
             for k in range(0, len(ts_code_df)):
                 ts_code = ts_code_df.loc[k]['ts_code']
-                sql = "select trade_date, close from fut_spread_daily where ts_code = '{}' order by ts_code".format(ts_code)
+                sql = "select trade_date, close, vol from fut_spread_daily where ts_code = '{}' order by ts_code".format(ts_code)
                 df = read_data(engine_ts, sql)
                 if k == 0:
                     num_of_trade_date = len(df)
@@ -45,7 +47,7 @@ def update_spread_config():
                 mad = np.median(abs(df['close'] - me))
                 up = me + (2*mad)
                 down = me - (2*mad)
-                df.drop(df[((df.close < down) | (df.close > up))].index, inplace=True)
+                df.drop(df[(((df.close < down) & (df.vol < 1000)) | ((df.close > up) & (df.vol < 1000)))].index, inplace=True)
                 close_df = pd.concat([close_df, df])
             num = len(close_df)
             if num == 0:
@@ -59,6 +61,7 @@ def update_spread_config():
             rec_spread = round((low + (high - low) * 0.1), 1)
             # print(close_df)
             # print(rec_spread)
+            # exit(1)
             # print(close_df.loc[max(round(num * 0.1), 1) - 1]['close'])
             # close_df['vol'].plot()
             # plt.axhline(y=np.nanmean(close_df['vol'])/2)
