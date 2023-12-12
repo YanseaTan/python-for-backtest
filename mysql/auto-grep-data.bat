63c2488eacb3b6ea16ff56a49ebb295ed0294a8c
Yansea Tan <yanseatan@163.com>
2023-12-12 17:15:07 +0800

    add postgre

--- mysql/auto-grep-data.bat
+++ mysql/auto-grep-data.bat
@@ -1,4 +1,8 @@
-@echo off  
+@REM @Author: Yansea
+@REM @Date:   2023-12-08
+@REM @Last Modified by:   Yansea
+@REM Modified time: 2023-12-12@echo off  
+
 C:  
 cd C:\Users\yanse\Desktop\repo\python-for-backtest
 start python ./mysql/tushare-data-to-mysql/TushareDataToMySQL.py
@@ -6,4 +10,6 @@ start python ./mysql/tfapi-data-to-mysql/TFAPIDataToMySQL.py
 start python ./mysql/tushare-data-to-mysql/FuturesMDToSpread.py
 start python ./mysql/tushare-data-to-mysql/UpdateSpreadConfig.py
 start python ./mysql/tushare-data-to-mysql/WriteSpreadDataToXlsx.py
+
+start python ./postgre/tfapi-data-to-postgre/TFAPIDataToPostgre.py
 exit
---
+++ postgre/tfapi-data-to-postgre/DatabaseTools.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: Yansea
+# @Date:   2023-10-18
+# @Last Modified by:   Yansea
+# @Last Modified time: 2023-12-12
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+# 创建指定数据库操作引擎
+def creat_engine_with_database(database):
+    engine_ts = create_engine('postgresql://postgres:shan3353@10.10.20.188:5432/{}?sslmode=disable'.format(database))
+    return engine_ts
+
+# 获取指定数据库的指定表格内容
+def read_data(engine_ts, sql):
+    df = pd.read_sql_query(sql, engine_ts)
+    return df
+
+# 将指定内容写入指定数据库的指定表格中
+def write_data(engine_ts, tableName, schemaName, df):
+    df.to_sql(tableName, engine_ts, schemaName, index=False, if_exists='append', chunksize=5000)
